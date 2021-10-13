05fbeb101406efd85fe6432b4d74c156731061e4
Edwar Garcia <Alexandergv04@gmail.com>
2021-10-13 18:15:12 -0400

    Agregar archivos de proyecto.

---
+++ PersonManager/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonManager.Models;
+
+namespace PersonManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.TablaP = new List<Person>();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Person model)
+        {
+            ViewBag.TablaP = Person.Persona(model);
+            ModelState.Clear();
+
+            return View();   
+        }  
+        [HttpPost]
+        public ActionResult DeleteRow(Person model)
+        {
+            ViewBag.TablaP = Person.DeleteRow(model);
+            ModelState.Clear();
+
+            return View("Index");
+        }
+        [HttpGet]
+        public ActionResult GetOlder()
+        {
+            ViewBag.TablaP = Person.GetOlder();
+            return View("Index");
+        }
+            [HttpGet]
+        public ActionResult GetCommon()
+        {
+            ViewBag.TablaP = Person.GetCommon();
+            return View("Index");
+        }            [HttpGet]
+        public ActionResult ShowAll()
+        {
+            ViewBag.TablaP = Person.Added;
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult UpdateRecord(Person Model)
+        {
+            ViewBag.TablaP = Person.UpdateRecord(Model);
+            ModelState.Clear();
+            return View("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PersonManager/Models/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace PersonManager.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo requerido.*")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Campo requerido.*")]
+        public int Age { get; set; }
+
+        //private static DataTable Added = new DataTable();
+        public static List<Person> Added = new List<Person>();
+
+        public static List<Person> Persona(Person person)
+        {
+            if (Added.Count > 0)
+            {
+                person.Id = Added.Last().Id + 1;
+            }    
+            //List<Person> listaP = new List<Person>();
+            Added.Add(person);
+            return Added;
+        }
+
+        public static List<Person> GetOlder()
+        {
+            List<Person> Oldest = new List<Person>();
+            if (Added.Count > 0)
+            {
+                int m = Added.Max(t => t.Age);
+                Oldest = Added.Where(t => t.Age == m).ToList();
+            }
+                return Oldest;
+        }
+        public static List<Person> DeleteRow(Person model)
+        {
+            var IdDel = Added.FindIndex(a => a.Id == model.Id);
+            Added.RemoveAt(IdDel);
+            return Added;
+        }
+        public static List<Person> GetCommon()
+        {
+            List<Person> Common = new List<Person>();
+            if (Added.Count > 0)
+            {
+                var m = Added.GroupBy(t => t.Age).ToList();
+                var rep = m.OrderByDescending(grp => grp.Count()).ToList().First().Count();
+                var listaRep = m.Where(gr => gr.Count() == rep).ToList();
+                var g = listaRep.Select(s => s.Key).ToList();
+                //int m = Added.GroupBy(t => t.Age).OrderByDescending(grp => grp.Count())
+                //    .Select(grp => grp.Key).First();
+                Common = Added.Where(t => g.Contains(t.Age)).OrderBy(gr => gr.Age).ToList();
+            }
+            return Common;
+        }
+
+        public static List<Person> UpdateRecord(Person person)
+        {
+            var Index = Added.Where(x => );
+            return Added;
+        }
+
+    }
+}
